6260db3505ac1a78ebf39f8226d0fddcaa58b017
Sam Weston <weston.sam@gmail.com>
2018-07-06 22:40:01 +0100

    Get flannel working. Cluster now functions! :D

--- master.sh
+++ master.sh
@@ -11,6 +11,12 @@ apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 curl -sSL https://get.docker.com/ | sh
 systemctl start docker
 
-kubeadm init --token=${k8stoken}
+kubeadm init --token=${k8stoken} --pod-network-cidr=10.244.0.0/16
 
-#kubectl apply -f https://git.io/weave-kube
+# Pass bridged IPv4 traffic to iptables chains (required by Flannel like the above cidr setting)
+echo "net.bridge.bridge-nf-call-iptables = 1" > /etc/sysctl.d/60-flannel.conf
+service procps start
+
+# Set up kubectl for the ubuntu user and Flannel
+mkdir -p /home/ubuntu/.kube && cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config && chown -R ubuntu. /home/ubuntu/.kube
+su -c 'kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml' ubuntu
--- worker.sh
+++ worker.sh
@@ -11,4 +11,8 @@ apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 curl -sSL https://get.docker.com/ | sh
 systemctl start docker
 
-for i in {1..50}; do kubeadm join --token=${k8stoken} --discovery-token-unsafe-skip-ca-verification ${masterIP} && break || sleep 15; done
+# Pass bridged IPv4 traffic to iptables chains (required by Flannel)
+echo "net.bridge.bridge-nf-call-iptables = 1" > /etc/sysctl.d/60-flannel.conf
+service procps start
+
+for i in {1..50}; do kubeadm join --token=${k8stoken} --discovery-token-unsafe-skip-ca-verification ${masterIP}:6443 && break || sleep 15; done
