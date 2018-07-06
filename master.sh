619e156648d2302a2bbef5bf65750daa2e2f3a85
Sam Weston <weston.sam@gmail.com>
2018-07-06 22:56:45 +0100

    A few more tweaks to the cluster config

--- master.sh
+++ master.sh
@@ -2,15 +2,21 @@
 swapoff -a
 sed -i '/swap/d' /etc/fstab
 
+# Install K8S and Kubeadm
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+# Install Docker, point at big ephemeral drive and turn on log rotation
 curl -sSL https://get.docker.com/ | sh
+mkdir /mnt/docker
+echo 'export DOCKER_OPTS="-g /mnt/docker --log-driver=json-file --log-opt=max-size=10m --log-opt=max-file=5"' >> /etc/default/docker
 systemctl start docker
 
+# Set up the cluster
 kubeadm init --token=${k8stoken} --pod-network-cidr=10.244.0.0/16
 
 # Pass bridged IPv4 traffic to iptables chains (required by Flannel like the above cidr setting)
@@ -20,3 +26,6 @@ service procps start
 # Set up kubectl for the ubuntu user and Flannel
 mkdir -p /home/ubuntu/.kube && cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config && chown -R ubuntu. /home/ubuntu/.kube
 su -c 'kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml' ubuntu
+
+# Allow pod scheduling on the master (no recommended but we're doing it anyway :D)
+su -c 'kubectl taint nodes --all node-role.kubernetes.io/master-' ubuntu
--- worker.sh
+++ worker.sh
@@ -2,17 +2,23 @@
 swapoff -a
 sed -i '/swap/d' /etc/fstab
 
+# Install K8S and Kubeadm
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+# Install Docker and point at big ephemeral drive
 curl -sSL https://get.docker.com/ | sh
+mkdir /mnt/docker
+echo 'export DOCKER_OPTS="-g /mnt/docker --log-driver=json-file --log-opt=max-size=10m --log-opt=max-file=5"' >> /etc/default/docker
 systemctl start docker
 
 # Pass bridged IPv4 traffic to iptables chains (required by Flannel)
 echo "net.bridge.bridge-nf-call-iptables = 1" > /etc/sysctl.d/60-flannel.conf
 service procps start
 
+# Join the cluster
 for i in {1..50}; do kubeadm join --token=${k8stoken} --discovery-token-unsafe-skip-ca-verification ${masterIP}:6443 && break || sleep 15; done
